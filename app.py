f3014a758d6f03b46c99e955d330670960480706
pqshosabettu126 <pqshosabettu@gmail.com>
2025-06-25 15:49:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ uploaded_file = st.file_uploader("Upload a chest X-ray image", type=["jpg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image",  use_container_width=True)
 
     # Preprocess
     input_tensor = transform(image).unsqueeze(0)
